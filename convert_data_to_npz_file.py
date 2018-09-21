9c141d1ef33dc3f6710d012be388fc6b2219b3d4
TristinGlunt <tglunt@unm.edu>
2018-09-20 18:31:15 -0600

    Updated sparse matrix file to include first line of data and remove the first column of indices.

--- convert_data_to_npz_file.py
+++ convert_data_to_npz_file.py
@@ -3,9 +3,10 @@ import scipy.sparse
 import numpy as np
 
 def main():
-	data = pd.read_csv("training.csv")
+	data = pd.read_csv("training.csv", header=None)
 	print("convert to values...")
 	data_values = data.values
+	data_values = data_values[:, 1:]
 	print("begin converting to csr_matrix...")
 	matrix_converted = scipy.sparse.csr_matrix(data_values)
 	# save the matrix to a file
--- main.py
+++ main.py
@@ -5,11 +5,21 @@ import numpy as np
 # indexing into sparse matrix:
 # https://stackoverflow.com/questions/24665269/how-do-you-edit-cells-in-a-sparse-matrix-using-scipy
 
+# https://stackoverflow.com/questions/38836100/accessing-sparse-matrix-elements
+
 def main():
 	#this should load in a csr_matrix
 	data = scipy.sparse.load_npz("sparse_matrix_convert.npz")
-	print("Value at (row 25, col 15): " + str(data[25, 15]))
-	print("test")
+	print("Shape of matrix: " + str(data.shape))
+	# appears to be 0-indexed, and pandas chopped off the first row, so we need to start at the second row, and i didn't chop off first column
+	print("Value at (row 1, col 12): " + str(data[0, 12]))
+	print("Value that is a 0: " + str(data[0, 1]))
+	print("Column M in Excel file: ")
+	print(data[:, 11])
+	print("First column in excel file:")
+	print(data[:, 0])
+	print("Entire data:")
+	print(data)
 
 if __name__ == "__main__":
 	main()
