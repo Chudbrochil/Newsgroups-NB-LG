72b0f0c410663789f20e5cf8b2a60c289a2cdb80
Tristin <tglunt@unm.edu>
2018-10-10 20:20:33 -0600

    Changed logistic regression predict to be a couple of lines

--- logistic_regression.py
+++ logistic_regression.py
@@ -146,23 +146,12 @@ def normalize_columns(Z):
 # Also prints the accuracy for the given data
 def lr_predict(X, W, Y):
     predictions = (W.dot(X.transpose())).expm1()
-    predictions = predictions.toarray()
 
-    max_value = -math.inf
-    max_index = -1
-    labels = []
+    maximum_index_for_each_example = new_data_dotted_likelihoods_plus_priors.argmax(axis=1)
 
-    # for every example
-    for j in range(predictions.shape[1]):
-        for i in range(20):
-            #print(str(i) + " : " + str(predictions[i][j]))
-            if predictions[i][j] > max_value:
-                max_value = predictions[i][j]
-                max_index = i+1
-        labels.append(max_index)
-        # print("")
-        max_value = -math.inf
-        max_index = -1
+    predictions = []
+    for index in maximum_indices_for_each_example:
+        predictions.append(index + 1)
 
     if Y != None:
         accuracy = 0
